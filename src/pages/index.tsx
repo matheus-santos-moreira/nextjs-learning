1b456ebd749769cc96a4b44e1b23811265350aca
Matheus Santos <matheussm301@gmail.com>
2020-10-28 00:34:01 -0300

    feat: Server side rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,44 @@
+import { GetServerSideProps } from 'next';
+import { useEffect, useState } from 'react';
 import {Title} from '../styles/pages/Home';
 
-export default function Home() {
+interface IProduct {
+  id: string;
+  title: string;
+}
+
+interface HomeProducts {
+  recommendedProducts: IProduct[];
+}
+
+export default function Home({recommendedProducts}: HomeProducts) {
+
   return (
     <div>
       <Title>Olá Mundo</Title>
+ 
+      <section>
+        <Title>Products</Title>
+
+        <ul>
+          {recommendedProducts.map(product => (
+            <li key={product.id} >
+              <h1>{product.title}</h1>
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   )
 }
+
+export const getServerSideProps: GetServerSideProps<HomeProducts> = async () => {
+  const response = await fetch('http://localhost:3333/recommended');
+  const recommendedProducts= await response.json();
+
+  return {
+    props: {
+      recommendedProducts
+    }
+  }
+  };
