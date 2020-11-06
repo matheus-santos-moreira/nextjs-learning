00b5508be248e75b68f3f080c2e0000aa5799174
Matheus Santos <matheussm301@gmail.com>
2020-11-06 08:15:55 -0300

    feat: Lazy load for components

---
+++ src/components/AddToCartModal.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AddToCartModal: React.FC = () => {
+  return (
+    <h1>Do you really want to add product to the cart?</h1>
+  );
+}
+
+export default AddToCartModal;
